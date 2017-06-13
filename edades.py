2131fd8f16e4561aa31e68208fba9bf94c20418a
jocsel <jocsel52@gmail.com>
2017-06-13 13:42:50 -0600

    Create edades.py

--- edades.py
+++ edades.py
@@ -1,7 +1,7 @@
 usuario=input('Ingrese el nombre del usuario:')
 edad=int(input('Ingresa tu edad actual:'))
 año=int(input('Ingresar el año actual:'))
-restante=190-edad
+restante=100-edad
 suma=restante+año
-print('En el año: ',suma,' cumpliras 190 años de haber nacido')
+print('En el año: ',suma,usuario,' cumpliras 190 años de haber nacido')
 
