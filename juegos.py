cfcc87ca14e7eb9248dc1c7a8cea9b921deb8516
jocsel <jocsel@gmail.com>
2017-06-14 17:54:15 -0600

    juego

---
+++ juegos.py
@@ -0,0 +1,25 @@
+from argparse import validador
+opciones={
+	'piedra':
+	{
+		'papel':False,
+	'	tijera':True,	
+	},
+	'papel':
+	{
+		'piedra':False,
+	'	tijera':True,	
+	},
+	'tijera':
+	{
+		'piedra':False,
+		'papel':True,	
+	},
+}
+i=0
+jugadores=int(input('Cuantos son los jugadores: '))
+while i<jugadores
+	j1=input('Eleccion del jugador #',(i+1),':')
+	if 
+	i+=1
+	
