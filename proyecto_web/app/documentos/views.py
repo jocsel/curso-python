825c1eb3073094db748b431c4effe9108084b3e3
jocsel <jocsel52@gmail.com>
2017-07-05 17:51:01 -0600

    adelanto 2 del proyecto_web

--- proyecto_web/app/documentos/views.py
+++ proyecto_web/app/documentos/views.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from documentos.models import Documento
 
 # Create your views here.
+from django.views.generic import View
+class Documentos(View):
+    def get(self, request, *args, **kwargs):
+        docs = Documento.objects.all()
+        #SELECT * FROM documentos_documentos
+        context = {
+            'docs':docs,
+            'encabezado': 'Mis Documentos'
+        }
+        return render(request,'documentos.html',context)
+
+
